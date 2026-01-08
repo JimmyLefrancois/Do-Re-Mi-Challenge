208087098a80d6ba36d687a255f3aaa0cda4b6fc
JimmyLefrancois <jimmylefrancois38@gmail.com>
2026-01-08 13:07:47 +0000

    fix: remove duplicate shuffleArray declaration (use imported helper)

--- script.js
+++ script.js
@@ -141,14 +141,7 @@ function handleTimeout() {
     }, 2000);
 }
 
-function shuffleArray(array) {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-}
+// Note: `shuffleArray` is imported from `src/game.js`.
 
 function createNoteButtons() {
     notesGrid.innerHTML = '';
